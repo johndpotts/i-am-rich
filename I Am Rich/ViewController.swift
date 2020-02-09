38d808f6633cdb312555cb0588de3686e1eef376
John Potts <jdpotts189@gmail.com>
2020-02-09 06:48:01 -0500

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by John Potts on 2/9/20.
+//  Copyright © 2020 johndpotts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
